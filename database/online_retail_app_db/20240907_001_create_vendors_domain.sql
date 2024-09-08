177e0888752e98d6da41015eccae286cd16fbd49
pavlinpetkov420 <pavlinpetkov420@gmail.com>
2024-09-08 14:49:52 +0300

    P1DP: Add DDL for vendors domain, update task tracker

--- database/online_retail_app_db/20240907_001_create_vendors_domain.sql
+++ database/online_retail_app_db/20240907_001_create_vendors_domain.sql
@@ -1,20 +1,39 @@
+-- Log In with SUPERUSER to create new schema and add it to search_path
+CREATE SCHEMA IF NOT EXISTS vendors AUTHORIZATION p1dp_ora_dba;
+
+SET search_path TO public, employees, vendors;
+
+SHOW search_path;
+
+-- Double check search path before create tables!
+
+-- Vendor
+-- vendor_id (PK): Unique identifier for the vendor.
+-- name: Name of the vendor.
+-- vendor_type_id (FK): Reference to the vendor_type table.
+-- contract_id (FK, can be NULL if no contract): Reference to the vendor_contracts table.
+-- phone_number: Contact phone number.
+-- primary_contact_name: Name of the primary contact person.
+-- created_by: User ID or name of the person who created the record.
+-- created_at: Timestamp when the record was created.
+-- updated_by: User ID or name of the person who last updated the record.
+-- updated_at: Timestamp when the record was last updated.
+
+CREATE TABLE vendors.vendor (
+    vendor_id uuid DEFAULT gen_random_uuid()
+    , vendor_name VARCHAR(100) NOT NULL 
+    , vendor_type_id uuid
+    , contract_id uuid
+    , phone_number VARCHAR(10) NOT NULL
+    , primary_contact_name VARCHAR(100) NOT NULL
+    , created_by VARCHAR(50)
+    , created_at TIMESTAMP DEFAULT clock_timestamp()
+    , updated_by VARCHAR(50)
+    , updated_at TIMESTAMP DEFAULT clock_timestamp()
+    , CONSTRAINT pk_vendor_id PRIMARY KEY (vendor_id)
+);
+
 /*
-Vendors will be separated in 4 tables as a start:
-
-Vendor Model
-
-vendor Table
-vendor_id (PK): Unique identifier for the vendor.
-name: Name of the vendor.
-vendor_type_id (FK): Reference to the vendor_type table.
-contract_id (FK, can be NULL if no contract): Reference to the vendor_contracts table.
-phone_number: Contact phone number.
-primary_contact_name: Name of the primary contact person.
-created_by: User ID or name of the person who created the record.
-created_at: Timestamp when the record was created.
-updated_by: User ID or name of the person who last updated the record.
-updated_at: Timestamp when the record was last updated.
-
 vendor_details Table
 vendor_id (PK, FK to vendor.vendor_id): Unique identifier for the vendor, also a foreign key to the vendor table.
 address: Address of the vendor.
@@ -34,8 +53,33 @@ preferred_delivery_method: Preferred method for receiving purchase orders.
 created_by: User ID or name of the person who created the record.
 created_at: Timestamp when the record was created.
 updated_by: User ID or name of the person who last updated the record.
-updated_at: Timestamp when the record was last updated.
+updated_at: Timestamp when the record was last updated
+*/
 
+CREATE TABLE vendors.vendor_details (
+    tax_id VARCHAR(100) -- PK
+    , vendor_id uuid -- FK REF to vendors
+    , address VARCHAR(100) NOT NULL
+    , email VARCHAR(100) NULL -- Add check constraint for email
+    , website_url VARCHAR(100) NULL
+    , primary_contact_position VARCHAR(50) NOT NULL
+    , secondary_contact_name VARCHAR(100) NULL
+    , secondary_contact_phone VARCHAR(100) NULL
+    , secondary_contact_email VARCHAR(100) NULL -- Again check constraint
+    , bank_account_number VARCHAR(50)
+    , bank_name VARCHAR(100)
+    , payment_terms VARCHAR(20) NOT NULL
+    , is_active BOOLEAN
+    , notes VARCHAR(500) NULL
+    , preferred_delivery_method VARCHAR(50) NULL
+    , created_by VARCHAR(50)
+    , created_at TIMESTAMP DEFAULT clock_timestamp()
+    , updated_by VARCHAR(50)
+    , updated_at TIMESTAMP DEFAULT clock_timestamp()
+    , CONSTRAINT pk_vendor_details PRIMARY KEY (tax_id)
+);
+
+/*
 vendor_type Table
 vendor_type_id (PK): Unique identifier for the vendor type.
 vendor_type: Name or category of the vendor type.
@@ -44,7 +88,20 @@ created_by: User ID or name of the person who created the record.
 created_at: Timestamp when the record was created.
 updated_by: User ID or name of the person who last updated the record.
 updated_at: Timestamp when the record was last updated.
+*/
 
+CREATE TABLE vendors.vendor_type (
+    vendor_type_id uuid DEFAULT gen_random_uuid()
+    , vendor_type VARCHAR(15) NOT NULL
+    , description VARCHAR(100) 
+    , created_by VARCHAR(50)
+    , created_at TIMESTAMP DEFAULT clock_timestamp()
+    , updated_by VARCHAR(50)
+    , updated_at TIMESTAMP DEFAULT clock_timestamp()
+    , CONSTRAINT pk_vendor_type_id PRIMARY KEY (vendor_type_id)
+);
+
+/*
 vendor_contracts Table
 contract_id (PK): Unique identifier for the contract.
 start_date: Start date of the contract.
@@ -56,38 +113,38 @@ updated_by: User ID or name of the person who last updated the record.
 updated_at: Timestamp when the record was last updated.
 */
 
--- Log In with SUPERUSER to create new schema and add it to search_path
-CREATE SCHEMA IF NOT EXISTS vendors AUTHORIZATION AUTHORIZATION p1dp_ora_dba;
-
-SET search_path "${user}, public, employees, vendors";
-
-SHOW search_path;
--- Double check search path before create tables!
-
--- Vendor
--- vendor_id (PK): Unique identifier for the vendor.
--- name: Name of the vendor.
--- vendor_type_id (FK): Reference to the vendor_type table.
--- contract_id (FK, can be NULL if no contract): Reference to the vendor_contracts table.
--- phone_number: Contact phone number.
--- primary_contact_name: Name of the primary contact person.
--- created_by: User ID or name of the person who created the record.
--- created_at: Timestamp when the record was created.
--- updated_by: User ID or name of the person who last updated the record.
--- updated_at: Timestamp when the record was last updated.
-CREATE TABLE vendors.vendor (
-    vendor_id uuid DEFAUT gen_random_uuid()
-    , vendor_name VARCHAR(100) NOT NULL 
-    , vendor_type_id uuid
-    , contract_id uuid
-    , phone_number VARCHAR(10)
-    , primary_contact_name VARCHAR(100)
+CREATE TABLE vendors.vendor_contracts (
+    contract_id uuid DEFAULT gen_random_uuid()
+    , start_date DATE NOT NULL 
+    , end_date DATE NOT NULL
+    , base_description VARCHAR(100) NOT NULL
     , created_by VARCHAR(50)
     , created_at TIMESTAMP DEFAULT clock_timestamp()
     , updated_by VARCHAR(50)
     , updated_at TIMESTAMP DEFAULT clock_timestamp()
-    , CONSTRAINT pk_vendor_id PRIMARY KEY (vendor_id)
+    , CONSTRAINT pk_vendor_contracts_id PRIMARY KEY (contract_id)
 );
 
+-- Foreign Keys
+-- vendors 
+ALTER TABLE vendors.vendor
+    ADD CONSTRAINT fk_vendor_vendor_type FOREIGN KEY (vendor_type_id) 
+    REFERENCES vendors.vendor_type (vendor_type_id);
 
--- TODO: Define FKs after all tables are created!
+ ALTER TABLE vendors.vendor
+    ADD CONSTRAINT fk_vendor_vendor_contracts FOREIGN KEY (contract_id)
+    REFERENCES  vendors.vendor_contracts (contract_id);
+
+-- vendor_details
+ALTER TABLE vendors.vendor_details
+    ADD CONSTRAINT fk_vendor_vendor_details FOREIGN KEY (vendor_id)
+    REFERENCES vendors.vendor (vendor_id);
+
+-- Check Constraints
+ALTER TABLE vendor_details
+ADD CONSTRAINT chk_email_format
+CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
+
+ALTER TABLE vendor_details
+ADD CONSTRAINT chk_secondary_email_format
+CHECK (secondary_contact_email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
